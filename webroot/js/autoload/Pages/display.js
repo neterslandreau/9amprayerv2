e1ac73306fee08eee0bdf314340920a01ca38b48
Niles Rowland <neterslandreau@gmail.com>
2013-02-14 21:28:37 -0500

    update layout to use unsemantic-grid.

---
+++ webroot/js/autoload/Pages/display.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	// alert('hola');
+	Gmap.populateMap('2725 N.E. 14th. Avenue Wilton Manors, FL 33334', 'First Christian Church of Wilton Manors');
+	$('#header-right').html('<div>This is new.</div>');
+});
