573e03a3cc5340455aec3dee53710af4a225178e
Niles Rowland <neterslandreau@gmail.com>
2013-02-21 21:03:14 -0500

    change autoload message.

--- webroot/js/autoload/Pages/display.js
+++ webroot/js/autoload/Pages/display.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	// alert('hola');
 	Gmap.populateMap('2725 N.E. 14th. Avenue Wilton Manors, FL 33334', 'First Christian Church of Wilton Manors');
-	$('#header-right').html('<div>Tip: Set an alarm for 9am on your cellphone to remind you.</div>');
+	$('#header-right').html('<div>Tip: Set an alarm for 9am on your cellphone to remind you what time it is.</div>');
 });
