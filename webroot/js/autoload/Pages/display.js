4c72462520629cc03ca3b57761bfba5b8d5d18a4
Niles Rowland <neterslandreau@gmail.com>
2013-03-13 16:50:21 -0400

    add credit link to the votd.

--- webroot/js/autoload/Pages/display.js
+++ webroot/js/autoload/Pages/display.js
@@ -8,10 +8,13 @@ $(document).ready(function() {
 
 	if (displayName == 'home') {
 		$.getJSON(App.basePath + 'feeds/get_votd.json', function(results, data) {
+			// console.log(results.guid);
 			var description = results.description.split('&#8212;');
 			var title = results.title.split(' - ');
 			$('#votd').append(
-				'<div class="votd-verse">'+description[0]+'</div><div class="votd-citation">'+title[1]+'</div>'
+				'<div class="votd-verse">'+description[0]+'</div>\n\
+				<div class="votd-citation">'+title[1]+'</div>\n\
+				<div class="votd-source"><a href="'+results.guid+'" target="_blank">More</a></div>'
 			);
 		});
 	}
